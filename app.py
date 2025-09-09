fcded135886493744078a9a6f477d3d974b706af
moosa113355 <moosanasir03@gmail.com>
2025-09-09 23:29:02 +0500

    Intentionally break code for CI failure

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ print ("Hello World")
 @app.route("/hello")
 def hello():
     return "Hello from Azure!"
+@app.route("/broken")
+def broken()   # <-- Notice the missing colon ':' here
+    return "Oops!"
